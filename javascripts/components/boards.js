98906f47b0283fc8f3c42627098f18051055c864
Michelle Beshears <meeshysu@gmail.com>
2018-10-23 19:44:00 -0500

    added some notes from second day of explanation

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -4,7 +4,7 @@ import { initializePinView } from '../components/pins.js';
 
 const bindEvents = () => {//added after, so dynamically onto the page
     $('#user-boards').on('click', '.board-card', (e) => {//board is the parent level of the card
-        const clickedBoardId = $(e.target).closest('.board-card').attr('id'); //e.target in a jquery object, but we don't need the image itself so closest. to access the string, you can use .attr with the id of what you want from your JSON file.
+        const clickedBoardId = $(e.target).closest('.board-card').attr('id'); //e.target in a jquery object, but we don't need the image itself so 'closest'. to access the string, you can use .attr with the id of what you want from your JSON file.
         $('#boards-page').hide();//this had a display:none connected to it (html) that enabled it to hide.
         $('#pins-page').show();
        // console.log(clickedBoardId);
@@ -15,7 +15,7 @@ const bindEvents = () => {//added after, so dynamically onto the page
 const writeBoards = (boards) => {
     let domString = '';
     boards.forEach(board => {
-        const boardImg = board.pins[0] ? board.pins[0].image_url : './db/default-img.jpeg';
+        const boardImg = board.pins[0] ? board.pins[0].image_url : './db/default-img.jpeg'; //first image of the first array.
         domString += `
     <div id='${board.id}' class="board-card p-2">
             <img class="card-img-top" src="${boardImg}" alt="Card image cap">
@@ -30,7 +30,7 @@ const writeBoards = (boards) => {
 }
 
 const initializeBoardView = () => {
-    loadBoards().then((boards) => {
+    loadBoards().then((boards) => { //first promise - loadboards. when it comes back successfully it returns an array of boards. once you have the array of boards you return the second promise loadpins that passes in the board. bc of the paranthesis it executes it. .then is actually on the loadpinsboards. it is in the resolve for that promise. that was done bc we had to pass boards correctly. 
         //console.log('inside the .then');
         // writeBoards(boards);//this to execute
         // bindEvents();//and this next to execute
